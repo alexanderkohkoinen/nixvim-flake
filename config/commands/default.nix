3beb8e1df29e5cad9cbb8cf2ab4488f5242ab171
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-03-14 21:07:34 +0100

    Added yank hg and noice plugin

--- config/commands/default.nix
+++ config/commands/default.nix
@@ -1,13 +1,18 @@
 {
-    autoCmd = [
-        {
-            event = "FileType";
-            pattern = [
-                "tex"
-                "latex"
-                "markdown"
-            ];
-            command = "setlocal spell spelllang=en_us";
-        }
-    ];
+  imports = [
+    ./editor.nix
+
+  ];
+
+  autoCmd = [
+    {
+      event = "FileType";
+      pattern = [
+        "tex"
+        "latex"
+        "markdown"
+      ];
+      command = "setlocal spell spelllang=en_us";
+    }
+  ];
 }
