b4d225906fec5790c8d4bd854a259c92a32e6179
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-03-14 20:22:16 +0100

    Added more lsps

--- config/lsp/default.nix
+++ config/lsp/default.nix
@@ -1,46 +1,53 @@
 { pkgs, ... }:
 {
-    imports = [
-        ./dotnet.nix
-        ./rust.nix
-    ];
+  imports = [
+    ./dotnet.nix
+    ./rust.nix
+  ];
 
-    plugins.lsp = {
-        enable = true;
-        lazyLoad = {
-            settings = {
-                inlayHints = true;
-                ft = [ "*" ];
-            };
-        };
-
-        servers = {
-            bashls.enable = true;
-            clangd.enable = true;
-            cmake.enable = true;
-            html.enable = true;
-            jsonls.enable = true;
-            lua_ls.enable = true;
-            marksman.enable = true;
-            nixd.enable = true;
-            yamlls.enable = true;
-        };
-
+  plugins.lsp = {
+    enable = true;
+    lazyLoad = {
+      settings = {
+        inlayHints = true;
+        ft = [ "*" ];
+      };
     };
 
-    # plugins.lsp-format = {
-    #     enable = true;
-    #     lazyLoad.settings.ft = [ "*" ];
-    # };
-
-    plugins.lsp-lines = {
-        enable = true;
-        lazyLoad.settings.ft = [ "*" ];
+    servers = {
+      bashls.enable = true;
+      clangd.enable = true;
+      cmake.enable = true;
+      dockerls.enable = true;
+      html.enable = true;
+      gopls.enable = true;
+      jsonls.enable = true;
+      lua_ls.enable = true;
+      marksman.enable = true;
+      nixd.enable = true;
+      pylsp.enable = true;
+      ruff.enable = true;
+      sqls.enable =true;
+      tailwindcss.enable =true;
+      ts_ls.enable = true;
+      yamlls.enable = true;
     };
 
-    plugins.lsp-signature = {
-        enable = true;
-        lazyLoad.settings.ft = [ "*" ];
-    };
+  };
+
+  plugins.lsp-format = {
+    enable = true;
+    lazyLoad.settings.ft = [ "nix" ];
+  };
+
+  plugins.lsp-lines = {
+    enable = true;
+    lazyLoad.settings.ft = [ "*" ];
+  };
+
+  plugins.lsp-signature = {
+    enable = true;
+    lazyLoad.settings.ft = [ "*" ];
+  };
 
 }
