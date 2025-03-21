676f1f3520f702d6afcea4012bfefd34a63c6039
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-03-21 14:45:09 +0100

    Minor change

--- config/coding/mini.nix
+++ config/coding/mini.nix
@@ -1,25 +1,40 @@
 {
-    plugins.mini.modules.ai = { };
-    plugins.mini.modules.pairs = {
-        modes = { 
-            insert = true;
-            command = false;
-            terminal = false;
-        };
-    };
+  config,
+  lib,
+  ...
+}:
+{
+  plugins.mini.modules =
+    {
+      ai = { };
 
-
-    plugins.mini.modules.surround = {
+      surround = {
         mappings = {
-            add = "gsa";
-            delete = "gsd";
-            find = "gsf";
-            find_left = "gsF";
-            highlight = "gsh";
-            replace = "gsr";
-            update_n_lines = "gsn";
+          add = "gsa";
+          delete = "gsd";
+          find = "gsf";
+          find_left = "gsF";
+          highlight = "gsh";
+          replace = "gsr";
+          update_n_lines = "gsn";
         };
-    };
 
+      };
+    }
+    // (
+      if !config.plugins.nvim-autopairs.enable then
+        {
+          pairs = {
+            modes = {
+              insert = true;
+              command = false;
+              terminal = false;
+            };
+
+          };
+
+        }
+      else
+        { }
+    );
 }
-
