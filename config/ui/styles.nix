8b3d6b44ddbb9d880546b6a28ee0bf005787f429
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-03-22 12:32:16 +0100

    Added transparent handler

--- config/ui/styles.nix
+++ config/ui/styles.nix
@@ -1,7 +1,45 @@
 {
-    colorschemes.rose-pine = {
-        enable = true;
-        lazyLoad.enable = true;
-    };
 
+  colorschemes.rose-pine = {
+    enable = true;
+    lazyLoad.enable = true;
+  };
+
+  plugins.transparent = {
+    enable = true;
+    settings = {
+      extra_groups = [
+        "BufferLineTabClose"
+        "BufferLineBufferSelected"
+        "BufferLineFill"
+        "BufferLineBackground"
+        "BufferLineSeparator"
+        "BufferLineIndicatorSelected"
+
+        "lualine"
+
+
+        "Snacks"
+        "SnacksNormal"
+        "SnacksBorder"
+      ];
+
+    };
+  };
+
+  keymaps = [
+    {
+      mode = "n";
+      action = "<CMD>TransparentToggle<CR>";
+      key = "<leader>uo";
+      options.desc = "Toggle transparent background";
+    }
+  ];
+
+  extraConfigLua = # Lua
+    ''
+      require('transparent').clear_prefix('BufferLine')
+      require('transparent').clear_prefix('lualine')
+      require('transparent').clear_prefix('Snacks')
+    '';
 }
